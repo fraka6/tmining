b0324cda31b067687947e6f1089ebbbb370b63b0
francis <francis@ubuntu.(none)>
2015-10-25 21:58:33 -0400

    add keras requirements

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@ alias pip=/home/$USER/anaconda/bin/pip
 alias ipython=/home/$USER/anaconda/bin/ipython
 pwd=`pwd`
 export PYTHONPATH=$PYTHONPATH:$pwd/mlboost
+#pip install -r requirements.txt
+#conda install --file conda-requirements.txt
