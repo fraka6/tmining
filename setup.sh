4deb29abf5a67314162b28388cc37260552d3868
francis <francis@ubuntu.(none)>
2015-10-26 02:29:59 -0400

    update setup.sh + add pytrade in requirements

--- setup.sh
+++ setup.sh
@@ -3,5 +3,7 @@ alias pip=/home/$USER/anaconda/bin/pip
 alias ipython=/home/$USER/anaconda/bin/ipython
 pwd=`pwd`
 export PYTHONPATH=$PYTHONPATH:$pwd/mlboost
+export PYTHONPATH=$PYTHONPATH:$pwd/pytrade
+export PYTHONPATH=$PYTHONPATH:$pwd
 #pip install -r requirements.txt
 #conda install --file conda-requirements.txt
