0b70f1c7752142479b07cfef3c246c72e3619040
Francis <fpieraut@gmail.com>
2015-10-27 11:00:47 -0400

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,9 @@ pwd=`pwd`
 export PYTHONPATH=$PYTHONPATH:$pwd/mlboost
 export PYTHONPATH=$PYTHONPATH:$pwd/pytrade
 export PYTHONPATH=$PYTHONPATH:$pwd
+# requirements
 #pip install -r requirements.txt
 #conda install --file conda-requirements.txt
+# other libs
+#hg clone https://bitbucket.org/fraka6/mlboost
+#git clone https://github.com/fraka6/pytrade.git
