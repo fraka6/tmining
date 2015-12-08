d18c003b4c2b7f962e13cb52340c71547ad57262
Francis Pieraut <francis@qmining.com>
2015-12-08 02:08:35 -0500

    update setup.sh

--- api/setup.sh
+++ api/setup.sh
@@ -1,3 +1,8 @@
+# install dependencies
 pip install --upgrade cython falcon
 pip install --upgrade gevent gunicorn
 pip install --upgrade httpie
+# run server
+gunicorn app:api &
+# request data
+http GET localhost:8000/bucket
